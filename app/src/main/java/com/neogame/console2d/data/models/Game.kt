004b638f94c72f71edd87877256efa6929480c39
omaymey15-sys <omaymey15@gmail.com>
2026-04-17 17:36:37 +0200

    Delete app/src/main directory

--- app/src/main/java/com/neogame/console2d/data/models/Game.kt
+++
@@ -1,9 +0,0 @@
-package com.neogame.console2d.data.models
-
-data class Game(
-    val name: String,
-    val filePath: String,
-    val description: String = "",
-    val thumbnail: String? = null,
-    val version: String = "1.0"
-)
