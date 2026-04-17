20b1e307dcf9be4b2ca6c60d4ffa25aa2967b92d
omaymey15-sys <omaymey15@gmail.com>
2026-04-17 18:25:06 +0200

    Create app/src/main/java/com/neogame/psp/utils/Constants.kt

---
+++ app/src/main/java/com/neogame/psp/utils/Constants.kt
@@ -0,0 +1,30 @@
+package com.neogame.psp.utils
+
+/**
+ * Constantes globales de l'application
+ */
+object Constants {
+    // App
+    const val APP_NAME = "ConsolePSP"
+    const val VERSION = "1.0.0"
+
+    // Storage
+    const val GAMES_DIR_NAME = "ConsolePSP"
+    const val ZIP_EXTENSION = "zip"
+    const val MIME_ZIP = "application/zip"
+
+    // Game Types
+    const val GAME_TYPE_2D = "2D"
+    const val GAME_TYPE_3D = "3D"
+    const val GAME_TYPE_4D = "4D"
+
+    // Object Types
+    const val OBJECT_TYPE_PLAYER = "player"
+    const val OBJECT_TYPE_ENEMY = "enemy"
+    const val OBJECT_TYPE_PLATFORM = "platform"
+    const val OBJECT_TYPE_TERRAIN = "terrain"
+    const val OBJECT_TYPE_COLLECTIBLE = "collectible"
+    const val OBJECT_TYPE_HAZARD = "hazard"
+    const val OBJECT_TYPE_PUZZLE = "puzzle_piece"
+    const val OBJECT_TYPE_NPC = "npc"
+}
