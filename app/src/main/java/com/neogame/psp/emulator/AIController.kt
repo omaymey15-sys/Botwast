bc4a4fb9ff5977854320b2dbf61a42f06a9958bd
omaymey15-sys <omaymey15@gmail.com>
2026-04-19 16:45:06 +0200

    Create app/src/main/java/com/neogame/psp/emulator/AIController.kt

---
+++ app/src/main/java/com/neogame/psp/emulator/AIController.kt
@@ -0,0 +1,13 @@
+package com.neogame.psp.emulator
+
+/**
+ * Contrôleur IA
+ * Gère l'IA des ennemis
+ */
+class AIController {
+    fun update(enemies: List<GameEngine.GameObject>) {
+        enemies.forEach { enemy ->
+            // IA simple - patrouille
+        }
+    }
+}
