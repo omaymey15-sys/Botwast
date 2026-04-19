20e2efffcabfcf395e0b80a4689bb04457b00847
omaymey15-sys <omaymey15@gmail.com>
2026-04-19 16:46:09 +0200

    Create app/src/main/java/com/neogame/psp/emulator/GameState.kt

---
+++ app/src/main/java/com/neogame/psp/emulator/GameState.kt
@@ -0,0 +1,13 @@
+package com.neogame.psp.emulator
+
+/**
+ * État du jeu
+ */
+class GameState {
+    var score = 0
+    var level = 1
+    var health = 100
+    var isGameOver = false
+    var isPaused = false
+    var lives = 3
+}
